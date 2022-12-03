17c07928cf80493c056bc7f1f3515193542de9eb
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-12-03 21:16:56 +0500

    feat: fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  try {
+    const file = await fs.readFile(filePath);
+    console.log(file.toString());
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
