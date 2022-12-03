5e9901bc0e1b89200e83d733136ccfda01b376b7
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-12-03 19:13:04 +0500

    feat: fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fresh.txt');
+  try {
+    await fs.writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
