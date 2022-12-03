720669e97d24eb7eacd89dfdafdc270fbaf7b00e
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-12-03 22:50:46 +0500

    fix: garbage code in rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -7,7 +7,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const rename = async () => {
   const oldPath = path.join(__dirname, 'files', 'wrongFilename.txt');
   const newPath = path.join(__dirname, 'files', 'properFilename.md');
-  const isNewFileExist = await fs.access(newPath);
   try {
     await fs.access(newPath);
     throw new Error('File exist');
