4914abffb8412c0d7021f5e5dd39556db0b36499
Alexandr Schepkivskii <SCHEPKIVSKII@gmail.com>
2022-12-03 21:24:30 +0500

    feat: fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+  const dirPath = path.join(__dirname, 'files');
+  try {
+    const files = await fs.readdir(dirPath);
+    console.log(files.toString());
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
