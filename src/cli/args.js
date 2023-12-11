114c6e6ace004acd25eda2272b57eac7c99b95a3
@a.schepkivsky <a.schepkivsky@iqdev.digital>
2023-12-11 12:51:36 +0500

    fix: cli args names

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,11 +1,15 @@
 import { argv } from 'node:process';
 const parseArgs = () => {
-    argv.slice(2).forEach((arg, index, arr) => {
+    const argsString = argv.slice(2).reduce((acc, arg, index, arr) => {
       if (arg.startsWith("--")) {
         const value = arr[index + 1];
-        console.log(`${arg} is ${value}`);
+        const argName = arg.slice(2);
+        return [...acc, `${argName} is ${value}`];
+      } else {
+        return acc;
       }
-    });
+    }, []).join(", ");
+    console.log(argsString);
 };
 
 parseArgs();
