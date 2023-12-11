d8aa19ea5451cfe1fef4a15c4370f110f0a900c5
@a.schepkivsky <a.schepkivsky@iqdev.digital>
2023-12-11 12:28:00 +0500

    fix: fs copy await

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,22 +1,23 @@
-import { copyFile, cp, mkdir, readdir } from 'node:fs/promises';
-import { join, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { copyFile, cp, mkdir, readdir } from "node:fs/promises";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const copy = async () => {
-    const source = join(__dirname, 'files');
-    const destination = join(__dirname, 'files_copy');
+  const source = join(__dirname, "files");
+  const destination = join(__dirname, "files_copy");
 
-    try {
-      await mkdir(destination);
-      const files = await readdir(source);
-      files.forEach(async (file) => {
-        copyFile(join(source, file), join(destination, file));
-      })
-      // cp(source, destination);
-    } catch (error) {
-      throw new Error('FS operation failed');
-    }
+  try {
+    const files = await readdir(source);
+    await mkdir(destination);
+    const copyPromises = files.map(async (file) =>
+      copyFile(join(source, file), join(destination, file))
+    );
+    await Promise.all(copyPromises);
+    // cp(source, destination);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
